ec47b922570681c5f3c833ee665d2b88cf31793f
Ugurcan Turk <ugurcan.uut82@gmail.com>
2021-07-07 01:25:58 +0300

    vue-cli search and click button

---
+++ Vue-cli/my-app/src/components/Search.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="search">
+    <input type="text" v-model="query" @keypress.enter="handleSearch"/>
+    <button v-on:click="handleSearch">Search</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            query: ""
+        }
+    },
+    methods: {
+        handleSearch(){
+            this.$emit("SearchRequested", this.query)
+        }
+    },
+}
+</script>
+
+<style>
+    .search input{
+        padding: 10px 20px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        width: 400px;
+    }
+    .search button {
+        padding: 15px 15px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+</style>
