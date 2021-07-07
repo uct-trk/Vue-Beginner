2cbe7bac87d14bf43fc657bfe9b1d09fe6ee3670
Ugurcan Turk <ugurcan.uut82@gmail.com>
2021-07-07 18:28:28 +0300

    vcloak, slots, nextTick, modifiers, filters

--- Vue-goodies/app.js
+++ Vue-goodies/app.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", () => {
 
-    window.vue = new Vue({
+    new Vue({
         el: "#vmodel",
         data: {
             message: "Hello world",
@@ -18,6 +18,83 @@ window.addEventListener("load", () => {
                 this.selectedVal = "baz"
             }
         },
+    })
+
+
+    setTimeout(() => {
+        new Vue({
+            el: "#vcloak",
+            data() {
+                return {
+                    users: [{ name: "Fatih" }, { name: "Ali" }]
+                }
+            },
+        })
+    }, 3000)
+
+    Vue.component("v-select", {
+        template: `
+        <select>
+            <slot></slot>
+        </select>
+        `
+    })
+
+    Vue.component("v-option", {
+        props: ["value"],
+        template: `
+        <option>{{value}}</option>
+        `
+    })
+
+    new Vue({
+        el: "#slots"
+    })
+
+    new Vue({
+        el: "#nextTick",
+        data() {
+            return {
+                message: "Hello"
+            }
+        },
+        methods: {
+            change() {
+                this.message = "Hello world"
+                console.log(document.querySelector("#nextTick").innerText)
+
+                this.$nextTick(() => {
+                    console.log(document.querySelector("#nextTick").innerText)
+                })
+            }
+        },
+    })
+
+    new Vue({
+        el: "#modifiers",
+        methods: {
+            log(){
+                console.log("Fooo")
+            }
+        },
+    })
+
+    new Vue({
+        el: "#filters",
+        data() {
+            return {
+                name: "logitech camera",
+                price: 25
+            }
+        },
+        filters: {
+            uppercase(text){
+                return text.toUpperCase();
+            },
+            withTax(price){
+                return `${(price * 1.18).toFixed(2)} TL`
+            }
+        }
     })
 })
 
