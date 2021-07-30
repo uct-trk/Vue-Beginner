c36d03f3948bea95827013ff0b472c4d38a90260
Ugurcan Turk <ugurcan.uut82@gmail.com>
2021-07-30 15:40:11 +0300

    Header component, Container component

--- instagram/src/App.vue
+++ instagram/src/App.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="app">
     <Header/>
+    <main class="main">
+      <Container>
     <router-view />
+    </Container>
+    </main>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Container from './components/Container.vue'
 export default {
   name: "App",
   components: {
-    Header
+    Header,
+    Container
   }
 }
 </script>
+
+<style scoped>
+  .main{
+    padding-top: 40px;
+  }
+</style>
