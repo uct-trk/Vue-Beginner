91986ade1ea57413956b1adf907f7abd74280f28
Ugurcan Turk <ugurcan.uut82@gmail.com>
2021-08-15 20:52:37 +0300

    created table and use axios

---
+++ table/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <Table v-if="tableDate" :tableDate="tableDate" :config="config" :style="{height: '600px'}"/>
+  </div>
+</template>
+
+<script>
+import Table from "./components/Table.vue";
+export default {
+  name: "App",
+  components: { Table },
+  data() {
+    return {
+      tableDate: undefined,
+      config: [
+        {
+          key: "avatar",
+          title: "Avatar",
+          type: "image"
+        },
+        {
+          key: "name",
+          title: "Name",
+          type: "text"
+        },
+        {
+          key: "companyName",
+          title: "Company",
+          type: "text"
+        },
+        {
+          key: "createdAt",
+          title: "Signup Date",
+          type: "date"
+        },
+      ],
+    };
+  },
+  mounted() {
+    this.$axios
+      .get("https://6119422f7b5c0f001759208d.mockapi.io/project123/users")
+      .then((res) => {
+        this.tableDate = res.data;
+      });
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ table/src/components/Table.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="awesome-table">
+    <table>
+      <thead>
+        <tr>
+          <th v-for="(obj, index) in config" :key="index">{{ obj.title }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, index) in tableDate" :key="index">
+          <td v-for="(obj, index) in config" :key="index">
+            <span v-if="obj.type === 'text'">{{ row[obj.key] }}</span>
+            <span v-if="obj.type === 'date'">{{
+              new Date(row[obj.key]).toLocaleDateString()
+            }}</span>
+            <figure v-if="obj.type === 'image'">
+              <img :src="row[obj.key]" />
+            </figure>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Table",
+  props: ["tableDate", "config"],
+};
+</script>
+
+<style scoped>
+.awesome-table {
+  border: 1px solid #999;
+  border-radius: 4px;
+  color: #333;
+  overflow: auto;
+}
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+th {
+  padding: 10px 5px;
+  position: sticky;
+  top: 0;
+  text-align: left;
+  background: #ccc;
+  border-bottom: 1px solid #999;
+}
+td {
+  padding: 5px 5px;
+  text-align: left;
+}
+figure {
+  margin-block-start: 0;
+  margin-block-end: 0;
+  margin-inline-end: 0;
+  margin-inline-start: 0;
+}
+img {
+  height: 60px;
+  border-radius: 100%;
+  overflow: hidden;
+  border: 1px solid #bbb;
+}
+</style>
